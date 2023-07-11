fbaf0c677168e1e55026984931eb9a7fd813ca84
Göktürk Fulser <gokturkfulser@gmail.com>
2023-07-11 15:49:55 +0300

    flask with html

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    name = request.args.get('name', 'Goko')  # Get the 'name' parameter from the request query string
-    return render_template('index.html', name=name)
-if __name__ == '__main__':
-    app.run()
+@app.route('/api/data')
+def get_data():
+    data = {
+        'name': 'World',
+        'message': 'Hello, World!'
+    }
+    return jsonify(data)
