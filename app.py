8a65ad6d005c7258275713b5605924c9fcdd423b
Göktürk Fulser <gokturkfulser@gmail.com>
2023-07-11 15:37:31 +0300

    Trying to connect backend with frontend

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Goko!'
+    name = request.args.get('name', 'Goko')  # Get the 'name' parameter from the request query string
+    return render_template('index.html', name=name)
 if __name__ == '__main__':
     app.run()
