74d095b5554a2926e2dbfece479abc14c97d1e87
Göktürk Fulser <gokturkfulser@gmail.com>
2023-07-12 17:43:28 +0300

    new trying with input system

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+"""from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
 if __name__ == '__main__':
-    app.run()
-
+    app.run()"""
 """from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -14,21 +13,21 @@ app = Flask(__name__)
 def hello():
     name = request.args.get('name', 'Goko')  # Get the 'name' parameter from the request query string
     return render_template('index.html', name=name)
-    
-    
-    
-    
-    <html>
-    <head>
-        <h1>Hello</h1>
-        <!-- head definitions go here -->
-    </head>
-    <body>
-        <h1>{{name}}</h1>
-        <!-- the content goes here -->
-    </body>
-</html>
+if __name__ == '__main__':
+    app.run(debug=True)
+"""
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        name = request.form.get('name', 'Goko')  # Get the 'name' value from the submitted form data
+    else:
+        name = 'Goko'  # Default value if 'name' is not provided
+        
+    return render_template('index.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
-    """
