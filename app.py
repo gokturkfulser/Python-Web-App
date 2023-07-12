6720573b6061790a3bba359450286684e921b3d2
Göktürk Fulser <gokturkfulser@gmail.com>
2023-07-12 17:02:41 +0300

    changing all print system to testing deploy

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+if __name__ == '__main__':
+    app.run()
+
+"""from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,5 +14,21 @@ app = Flask(__name__)
 def hello():
     name = request.args.get('name', 'Goko')  # Get the 'name' parameter from the request query string
     return render_template('index.html', name=name)
+    
+    
+    
+    
+    <html>
+    <head>
+        <h1>Hello</h1>
+        <!-- head definitions go here -->
+    </head>
+    <body>
+        <h1>{{name}}</h1>
+        <!-- the content goes here -->
+    </body>
+</html>
+
 if __name__ == '__main__':
     app.run(debug=True)
+    """
